25d3a2ce6744430d17507251c20f543e037af40e
Hitesh Agarwal <hitesh.agarwal@timesinternet.in>
2020-03-01 19:19:06 +0530

    Add Alternate app icon project folder

---
+++ AlternateAppIcons/AlternateAppIcons/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AlternateAppIcons
+//
+//  Created by Hitesh Agarwal on 01/03/20.
+//  Copyright © 2020 Hitesh Agarwal. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
